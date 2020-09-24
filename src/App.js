003f490b47aa8a7d5bf4d0e68afe5a056d547dbd
lazalaki <lakierbes@gmail.com>
2020-09-24 21:39:36 +0200

    Banner

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
+import Banner from "./Banner";
 
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchTrending} />
-      <Row title="Trending now" fetchUrl={requests.fetchTopRated} />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchTrending}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      {/* <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Hrorror Movies" fetchUrl={requests.fetchHorrorMovies} /> */}
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -5,7 +5,7 @@ import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const Row = ({ title, fetchUrl }) => {
+const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -16,16 +16,16 @@ const Row = ({ title, fetchUrl }) => {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
-
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
           <img
-            className="row__poster"
-            src={`${base_url}${movie.poster_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
             key={movie.id}
           />
