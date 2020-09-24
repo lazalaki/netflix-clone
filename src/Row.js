878d0781a98fa13ff20b1aeff00e8a8088c5bc8c
lazalaki <lakierbes@gmail.com>
2020-09-24 17:14:49 +0200

    Displaying movie posters

--- src/Row.js
+++ src/Row.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 
+import "./Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
 
@@ -13,10 +17,20 @@ const Row = ({ title, fetchUrl }) => {
   }, [fetchUrl]);
 
   console.log(movies);
+
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
-      {/* container -> posters */}
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+            key={movie.id}
+          />
+        ))}
+      </div>
     </div>
   );
 };
