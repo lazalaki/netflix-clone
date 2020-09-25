2e12ff08bbb3b9d516b8ed6b8690752425ae34d4
lazalaki <lakierbes@gmail.com>
2020-09-25 10:04:36 +0200

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./requests";
 import Banner from "./Banner";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
@@ -15,9 +17,9 @@ function App() {
       />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      {/* <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Hrorror Movies" fetchUrl={requests.fetchHorrorMovies} /> */}
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      {/* <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} /> */}
+      {/* <Row title="Hrorror Movies" fetchUrl={requests.fetchHorrorMovies} /> */}
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+import "./Navbar.css";
+import Avatar from "./img/netflix.png";
+
+const Navbar = () => {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  console.log(show);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
+        alt="Netflix Logo"
+        className="nav__logo"
+      />
+      <img src={Avatar} alt="Netflix Logo" className="nav__avatar" />
+    </div>
+  );
+};
+
+export default Navbar;
